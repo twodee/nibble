b49aecf5283158869869e3c719674c6a3b0037a3
Chris Johnson <crjjrc@gmail.com>
2020-04-05 13:09:43 -0500

    Jump only when on a right-colored block.

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -7,9 +7,13 @@ public class PlayerController : MonoBehaviour {
   public float speed;
 
   private new Rigidbody2D rigidbody;
+  private LayerMask mask;
+  private Collider2D[] hits;
 
   void Start() {
     rigidbody = GetComponent<Rigidbody2D>();
+    mask = LayerMask.GetMask(color);
+    hits = new Collider2D[10];
   }
 
   void Update() {
@@ -19,7 +23,17 @@ public class PlayerController : MonoBehaviour {
 
   void FixedUpdate() {
     if (Input.GetButtonDown($"{color} Vertical")) {
-      rigidbody.AddForce(Vector3.up * 200);
+      Vector2 footPosition = new Vector2(transform.position.x, transform.position.y - 0.25f);
+      int hitCount = Physics2D.OverlapCircleNonAlloc(footPosition, 0.01f, hits, mask);
+      bool isHit = false;
+      int i = 0;
+      while (i < hitCount && !isHit) {
+        isHit = hits[i].CompareTag("Block");
+        i += 1;
+      }
+      if (isHit) {
+        rigidbody.AddForce(Vector3.up * 200);
+      }
     }
   }
 }
